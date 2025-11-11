fe4c4f85e665b8dc37e80308b6369a0d4732c4ab
TiredSkitarii <jakob.mclaughlin@googlemail.com>
2025-11-11 21:36:54 +0000

    error fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,11 +55,12 @@ function gameTurn() {
 }
 
 function playerTurn() {
-    let i = game.playerTurn.length -1;
+    let i = game.playerTurn.length - 1;
     if (game.currentTurn[i] === game.playerTurn[i]) {
-        if (game.currentTurn.length == game.playerTurn.length)
+        if (game.currentTurn.length === game.playerTurn.length)
             game.score++;
         showScore();
+        alert("Well done!");
         addTurn();
     } else {
         alert ("Sorry, Wrong Move!")
